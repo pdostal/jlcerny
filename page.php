c70498ff8bb7a4aee6d5b1b0ca559af3038d045f
quiiicq <git@pdostal.cz>
2013-07-06 18:32:30 +0200

    Page header removed.

--- page.php
+++ page.php
@@ -4,7 +4,6 @@
 <section id="main" class="<?php echo get_post_type( get_the_ID() ); ?>">
 <?php if (have_posts()) { while (have_posts()) { the_post(); ?>
 	<article>
-		<header><h1><a href="<?php the_permalink(""); ?>"><?php the_title(); ?></a></h1></header>
 		<div class="content"><?php the_content(__('(Whole article...)')); ?></div>
 	</article>
 <?php } } else { ?>
